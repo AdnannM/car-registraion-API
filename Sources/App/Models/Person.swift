a5b891162f964e09f9bd305670192289b30f8e27
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-10-24 18:53:21 +0200

    Add Person Data

---
+++ Sources/App/Models/Person.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adnann Muratovic on 24.10.21.
+//
+
+import Foundation
+import Vapor
+
+struct Person {
+    var id: Int?
+    var name: String?
+    var lastName: String?
+    var dateOfBirth: String?
+    var jmbg: Int?
+    var city: String?
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,31 @@
 import Fluent
 import Vapor
+import RoutingKit
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+        return req.view.render("home")
     }
+    
+    
+//    func setupRoutes() throws {
+//        app.get("person") { req  in
+//
+//            let adnann = Person(from: Person(id: 1, name: "Adnann", lastName: "Muratovic", dateOfBirth: "07-04-1992", jmbg: 123125513513, city: "Banja Luka"))
+//
+//            return adnann
+//        }
+//    }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
+//    app.get("person") { req  in
+//
+//        let adnann = Person(from: Person(id: 1, name: "Adnann", lastName: "Muratovic", dateOfBirth: "07-04-1992", jmbg: 123125513513, city: "Banja Luka"))
+//
+//        var json = JSON()
+//        try json.set("name", adnann.name)
+//        return adnann.name
 
     try app.register(collection: TodoController())
 }
+
+
